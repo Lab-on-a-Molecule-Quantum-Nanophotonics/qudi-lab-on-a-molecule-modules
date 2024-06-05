699c5dd09b30cb93e536709b57c6ee1c756bbf55
hugoloup.levy-falk <hugoloup.levy-falk@ino.cnr.it>
2024-06-05 10:36:53 +0200

    feat: WIP moglabs integration,

--- src/qudi/gui/ni_laser_scanner/laser_scanner_settings.py
+++ src/qudi/gui/ni_laser_scanner/laser_scanner_settings.py
@@ -20,15 +20,19 @@ class LaserScanningSettingsMainWindow(QtWidgets.QDialog):
         self.exposureSpinBox = QtWidgets.QDoubleSpinBox()
         self.exposureSpinBox.setSuffix(" s")
         self.resetWavemeterButton = QtWidgets.QPushButton("Reset wavemeter")
-        self.manualSpinBox = QtWidgets.QDoubleSpinBox()
-        self.manualSpinBox.setSuffix(" V")
-        self.manualSpinBox.setDecimals(3)
+        self.calibrateButton = QtWidgets.QPushButton("Calibrate grating")
+        self.manualTensionSpinBox = QtWidgets.QDoubleSpinBox()
+        self.manualTensionSpinBox.setSuffix(" V")
+        self.manualTensionSpinBox.setDecimals(3)
+        self.centralWavelengthComboBox = QtWidgets.QComboBox()
         self.scanFormLayout.addRow("Minimum piezo tension", self.scanMinSpinBox)
         self.scanFormLayout.addRow("Maximum piezo tension", self.scanMaxSpinBox)
         self.scanFormLayout.addRow("Number of points", self.sampleSizeSpinBox)
         self.scanFormLayout.addRow("Exposure", self.exposureSpinBox)
         self.manualLayout.addRow("", self.resetWavemeterButton)
-        self.manualLayout.addRow("Output", self.manualSpinBox)
+        self.manualLayout.addRow("", self.calibrateButton)
+        self.manualLayout.addRow("Central wavelength", self.centralWavelengthComboBox)
+        self.manualLayout.addRow("Piezo tension", self.manualTensionSpinBox)
         self.manualGroupBox.setLayout(self.manualLayout)
         self.scanGroupBox.setLayout(self.scanFormLayout)
         self.layout.addWidget(self.scanGroupBox)
@@ -46,9 +50,12 @@ class LaserScanningSettingsGui(GuiBase):
         self._mw.scanMinSpinBox.setMaximum(scan_range[1])
         self._mw.scanMaxSpinBox.setMinimum(scan_range[0])
         self._mw.scanMaxSpinBox.setMaximum(scan_range[1])
-        self._mw.manualSpinBox.setMinimum(scan_range[0])
-        self._mw.manualSpinBox.setMaximum(scan_range[1])
-        self._mw.manualSpinBox.setSingleStep(0.001)
+        self._mw.manualTensionSpinBox.setMinimum(scan_range[0])
+        self._mw.manualTensionSpinBox.setMaximum(scan_range[1])
+        self._mw.manualTensionSpinBox.setSingleStep(0.001)
+        available_wavelengths = logic.get_calibration()
+        for w in available_wavelengths:
+            self._mw.centralWavelengthComboBox.addItem(str(w))
         
         sample_size_constraints = logic.sample_size_constraints()
         self._mw.sampleSizeSpinBox.setMinimum(sample_size_constraints[0])
@@ -60,17 +67,29 @@ class LaserScanningSettingsGui(GuiBase):
         self._mw.scanMinSpinBox.setValue(logic.scan_min)
         self._mw.scanMaxSpinBox.setValue(logic.scan_max)
         self._mw.sampleSizeSpinBox.setValue(logic.sample_size)
-        self._mw.manualSpinBox.setValue(logic.get_setpoint())
+        self._mw.manualTensionSpinBox.setValue(logic.get_tension())
         self._mw.exposureSpinBox.setValue(logic.get_exposure())
         
         self._mw.scanMinSpinBox.valueChanged.connect(logic.set_scan_min)
         self._mw.scanMaxSpinBox.valueChanged.connect(logic.set_scan_max)
-        self._mw.manualSpinBox.valueChanged.connect(logic.set_setpoint)
+        self._mw.manualTensionSpinBox.valueChanged.connect(logic.set_tension)
         self._mw.sampleSizeSpinBox.valueChanged.connect(logic.set_sample_size)
         self._mw.exposureSpinBox.valueChanged.connect(logic.set_exposure)
+        self._mw.centralWavelengthComboBox.currentTextChanged.connect(self.send_new_wavelength)
         self._mw.resetWavemeterButton.clicked.connect(logic.set_continuous_reading)
+        self._mw.calibrateButton.clicked.connect(logic.calibrate)
+        logic.calibrated.connect(self.update_available_central_wavelength)
         self._mw.show()
-
+        
+        
+    def update_available_central_wavelength(self, wavelengths):
+        self._mw.centralWavelengthComboBox.clear()
+        for w in wavelengths:
+            self._mw.centralWavelengthComboBox.addItem(str(w))
+            
+    def send_new_wavelength(self, wavelength_str):
+        self._scanning_settings_logic().set_center_wavelength(int(wavelength_str))
+            
     def on_deactivate(self):
         self._mw.scanMinSpinBox.valueChanged.disconnect()
         self._mw.scanMaxSpinBox.valueChanged.disconnect()
--- src/qudi/hardware/interfuse/ni_moglabs_spectrometer_interfuse.py
+++ src/qudi/hardware/interfuse/ni_moglabs_spectrometer_interfuse.py
@@ -1,24 +1,41 @@
 from typing import Iterable, Mapping, Union, Optional, Tuple, Type, Dict
 
+import time
 import numpy as np
 
 from qudi.interface.spectrometer_interface import SpectrometerInterface
 from qudi.interface.simple_scanner_interface import SimpleScannerInterface, SimpleScannerSettings, SimpleScannerConstraints
 from qudi.interface.finite_sampling_io_interface import SamplingOutputMode
+from qudi.interface.process_control_interface import ProcessControlConstraints, ProcessControlInterface
 from qudi.core.connector import Connector
 from qudi.core.statusvariable import StatusVar
 from qudi.core.configoption import ConfigOption
+from qudi.util.overload import OverloadedAttribute
 
-class NIMOGLabsSpectrometerInterfuse(SpectrometerInterface, SimpleScannerInterface):
+from qtpy import QtCore
+
+
+_Real = Union[int, float]
+
+class NIMOGLabsSpectrometerInterfuse(SpectrometerInterface, SimpleScannerInterface, ProcessControlInterface):
     _ni_finite_sampling_io = Connector(name='scan_hardware', interface='FiniteSamplingIOInterface')
     _ni_setpoint = Connector(name="setpoint_hardware", interface="ProcessSetpointInterface")
     _moglabs_laser = Connector(name='wavelength_hardware', interface='FiniteSamplingInputInterface')
+    _moglabs_grating = Connector(name='grating', interface='MotorInterface')
     
     _ni_output_channel = ConfigOption(name='analog_output_channel', missing='error')
     _ni_input_channel = ConfigOption(name='counter_input_channel', missing='error')
+    _grating_axis = ConfigOption(name='grating_axis', default='grating')
+    _mini_tension = ConfigOption(name='tension_min', default=-2.5)
+    _maxi_tension = ConfigOption(name='tension_max', default=2.5)
     
     _sample_rate = StatusVar(name="sample_rate", default=1e3)
     _number_of_samples = StatusVar(name="number_of_samples", default=2**12)
+    _wavelengths = StatusVar(name='frequencies', default=np.zeros(0))
+    _grating_positions = StatusVar(name='grating_positions', default=np.zeros(0))
+    
+    sigCalibrationChanged = QtCore.Signal()
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -35,6 +52,8 @@ class NIMOGLabsSpectrometerInterfuse(SpectrometerInterface, SimpleScannerInterfa
             sample_rate = self._sample_rate,
             number_of_samples = self._number_of_samples,
         )
+        if len(self._wavelengths) <= 0:
+            self.calibrate()
     def on_deactivate(self):
         self._sample_rate = self._settings.sample_rate
         self._number_of_samples = self._settings.number_of_samples
@@ -56,6 +75,8 @@ class NIMOGLabsSpectrometerInterfuse(SpectrometerInterface, SimpleScannerInterfa
         self.settings.sample_rate = 1/value
         
     # SimpleScannerInterface
+    constraints = OverloadedAttribute()
+    @constraints.overload("SimpleScannerInterface")
     @property
     def constraints(self):
         return self._constraints
@@ -63,7 +84,7 @@ class NIMOGLabsSpectrometerInterfuse(SpectrometerInterface, SimpleScannerInterfa
     def settings(self):
         return self._settings
     def set_settings(self, settings: SimpleScannerSettings) -> Tuple[bool, Iterable[str]]:
-        success, errors = settings.is_compatible_with(self.constraints)
+        success, errors = settings.is_compatible_with(self._constraints)
         if success:
             self._settings = settings
             return True,[]
@@ -86,17 +107,106 @@ class NIMOGLabsSpectrometerInterfuse(SpectrometerInterface, SimpleScannerInterfa
         self.log.info("returning to manual value")
         self.set_setpoint(self.get_setpoint())
         return d_wavemeter["wavelength"][wavelength_perm], d_ni_card[self._ni_input_channel][wavelength_perm]
-    def get_setpoint(self) -> float:
-        self._ni_setpoint().set_activity_state(self._ni_output_channel, True)
-        return self._ni_setpoint().get_setpoint(self._ni_output_channel)
-    def set_setpoint(self, value: float) -> None:
-        self._ni_setpoint().set_activity_state(self._ni_output_channel, True)
-        self._ni_setpoint().set_setpoint(self._ni_output_channel, value)
     def reset_continuous(self):
         self._moglabs_laser().stop_buffered_acquisition()
         self._ni_finite_sampling_io().stop_buffered_acquisition()
+    
+    @QtCore.Slot()
+    def calibrate(self):    
+        mod_status = self.set_setpoint("tension", 0)
+        constraints = self._moglabs_grating().get_constraints()[self._grating_axis]
+        self._motor_positions = np.arange(start=constraints['pos_min'], stop=constraints['pos_max'], step=constraints['pos_step'])
+        self._wavelengths = np.empty(len(self._motor_positions), dtype=np.float64)
+        self.log.info("Starting calibration scan...")
+        self._moglabs_laser().start_stream()
+        for i in range(len(self._motor_positions)):
+            self._moglabs_grating().move_abs({self._grating_axis: self._motor_positions[i]})
+            while self._moglabs_grating().get_pos()[self._grating_axis] != self._motor_positions[i]:
+                time.sleep(0.1)
+            self._wavelengths[i] = self._moglabs_laser().read_data(10)[0].mean()
+        self._moglabs_laser().stop_stream()
+        self.log.info("Calibration scan done.")
+        self.sigCalibrationChanged.emit()
+        
+    def get_calibration(self):
+        return self._grating_positions, self._wavelengths
     
     # Other
     @property
     def scan_step(self):
         return (self.settings.last_point - self.settings.first_point) / self.settings.number_of_samples
+
+    # ProcessControlInterface
+    @constraints.overload("ProcessControlInterface")
+    @property
+    def constraints(self):
+        return ProcessControlConstraints(
+            setpoint_channels = ["grating wavelength", "tension", "grating position setpoint"],
+            process_channels = ["wavelength", "grating position"],
+            units = {
+                "grating wavelength": "nm",
+                "tension": "V",
+                "grating position setpoint": "step",
+                "wavelength": "nm",
+                "grating position": "step"
+            },
+            limits = {
+                "grating wavelength": (min(self._wavelengths), max(self._wavelengths)),
+                "tension": (self._mini_tension, self._maxi_tension),
+                "grating position setpoint": (min(self._grating_positions), max(self._grating_positions)),
+                "wavelength": (400.0,2000.0),
+                "grating position": (min(self._grating_positions), max(self._grating_positions))
+            },
+            dtypes = {
+                "grating wavelength": np.float64,
+                "tension": np.float64,
+                "grating position setpoint": int,
+                "wavelength": np.float64,
+                "grating position": int
+            }
+        )
+        
+    def set_activity_state(self, channel: str, active: bool) -> None:
+        pass
+        
+    def get_activity_state(self, channel: str) -> bool:
+        return True
+        
+    def set_setpoint(self, channel: str, value: _Real) -> None:
+        if channel == "grating wavelength":
+            self.set_setpoint(
+                "grating position setpoint", 
+                self._motor_positions[np.argmin(np.abs(self._wavelengths-value))][0]
+            )
+        elif channel == "tension":
+            self._ni_setpoint().set_activity_state(self._ni_output_channel, True)
+            self._ni_setpoint().set_setpoint(self._ni_output_channel, value)
+        elif channel == "grating position setpoint":
+            self._moglabs_grating().move_abs({self._grating_axis: value})
+        else:
+            raise ValueError("No channel named %s" % channel)
+        
+    def get_setpoint(self, channel: str):
+        if channel == "grating wavelength":
+            return self._wavelengths[np.argmin(np.abs(self._motor_positions-self.get_setpoint("grating position setpoint")))][0]
+        elif channel == "tension":
+            if self._ni_setpoint().get_activity_state(self._ni_output_channel):
+                return self._ni_setpoint().get_setpoint(self._ni_output_channel)
+            else:
+                return 0.0
+        elif channel == "grating position setpoint":
+            return self._moglabs_grating().get_pos()[self._grating_axis]
+        else:
+            raise ValueError("No channel named %s" % channel)
+            
+    def get_process_value(self, channel: str) -> _Real:
+        """ Get current process value for a single channel """
+        if channel == "wavelength":
+            self._moglabs_laser().start_stream()
+            v = self._moglabs_laser().read_data(10)[0].mean()
+            self._moglabs_laser().stop_stream()
+            return v
+        elif channel == "grating position":
+            return self._moglabs_grating().get_pos()[self._grating_axis]
+        else:
+            raise ValueError("No channel named %s" % channel)
