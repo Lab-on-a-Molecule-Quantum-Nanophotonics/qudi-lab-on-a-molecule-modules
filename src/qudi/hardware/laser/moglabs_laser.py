7739c136e74b1ac5579b4f5eb420b1d79d24a503
Hugo Levy-Falk <hugo.levyfalk@lens.unifi.it>
2024-07-24 16:45:47 +0200

    feat: added current monitoring for the ldd.

--- src/qudi/hardware/laser/moglabs_laser.py
+++ src/qudi/hardware/laser/moglabs_laser.py
@@ -48,15 +48,28 @@ from qudi.util.enums import SamplingOutputMode
 
 from qudi.hardware.laser.moglabs_helper import MOGLABSDeviceFinder
 
-class MOGLABSMotorizedLaserDriver(SwitchInterface, ProcessControlInterface):
+class MOGLABSMotorizedLaserDriver(SwitchInterface, ProcessControlInterface, DataInStreamInterface):
     """
     Control the Laser diode driver directly.
     """
     port = ConfigOption("port")
+    default_buffer_size = ConfigOption("buffer_size", 2**16)
+    poll_time = StatusVar('poll_time', 0.01)
+    
+    _threaded = True
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.serial = serial.Serial()
+        self._lock = Mutex()
+        self._data_buffer: Optional[np.ndarray] = None
+        self._timestamp_buffer: Optional[np.ndarray] = None
+        self._current_buffer_position = 0
+        self.buffer_size = 0
+        self._watchdog_active = False
+        self._time_start = time.monotonic()
+        self._time_last_read = time.monotonic() 
+        self._constraints: Optional[DataInStreamConstraints] = None
 
     # Qudi activation / deactivation
     def on_activate(self):
@@ -66,14 +79,260 @@ class MOGLABSMotorizedLaserDriver(SwitchInterface, ProcessControlInterface):
         self.serial = device_finder.ldd
         self._ramp_running = False
         self._ramp_halt = 0.0
-        self._lock = Mutex()     
         self.serial.open()
         self._set_ramp_status("OFF")
+        self.buffer_size = self.default_buffer_size
+        self._constraints = DataInStreamConstraints(
+            channel_units = {
+                'current': 'A',
+            },
+            sample_timing=SampleTiming.TIMESTAMP,
+            streaming_modes = [StreamingMode.CONTINUOUS],
+            data_type=np.float64,
+            channel_buffer_size=ScalarConstraint(default=2**16,
+                                                 bounds=(2, (2**32)//10),
+                                                 increment=1,
+                                                 enforce_int=True),
+            sample_rate=ScalarConstraint(default=1/self.poll_time,bounds=(1,100),increment=1),
+        )
 
     def on_deactivate(self):
         """Deactivate module.
         """
+        self._stop_continuous_read()
+        time.sleep(self.poll_time)
         self.serial.close()
+        
+    # Internal management of buffers
+    def _prepare_buffers(self):
+        self._data_buffer = np.empty(self.buffer_size, dtype=np.float64)
+        self._timestamp_buffer = np.empty(self.buffer_size, dtype=np.float64)
+        self._current_buffer_position = 0
+    @QtCore.Slot()
+    def _start_continuous_read(self):
+        self._watchdog_active = True
+        if self.thread() is not QtCore.QThread.currentThread():
+            QtCore.QMetaObject.invokeMethod(self, '_continuous_read_callback', QtCore.Qt.BlockingQueuedConnection)
+        else:
+            self._time_start = time.monotonic()
+            self._time_last_read = self._time_start
+            self._continuous_read_callback()
+    @QtCore.Slot()
+    def _stop_continuous_read(self):
+        self._watchdog_active = False
+    @QtCore.Slot()
+    def _continuous_read_callback(self):
+        time_start = time.perf_counter()
+        try:
+            with self._lock:
+                t = time.monotonic()
+                delta_t = t - self._time_last_read
+                if delta_t < self.poll_time:
+                    postpone = int(round(1000*max(0, self.poll_time-delta_t)))
+                    QtCore.QTimer.singleShot(postpone, self._continuous_read_callback)
+                    return
+                self._time_last_read = t
+                try:
+                    val = self._get_current()
+                except ValueError:
+                    val = np.nan
+                if self._current_buffer_position < len(self._data_buffer):
+                    i = self._current_buffer_position
+                    self._data_buffer[i] = val * 1e-3
+                    self._timestamp_buffer[i] = t - self._time_start
+                    if i > 0 and self._timestamp_buffer[i-1] > self._timestamp_buffer[i]:
+                        self._timestamp_buffer[i] = self._timestamp_buffer[i-1]
+                    self._current_buffer_position += 1
+                else:
+                    pass
+        except:
+            self.log.exception("")
+        if self._watchdog_active:
+            overhead_time = time.perf_counter() - time_start
+            QtCore.QTimer.singleShot(int(round(1000*max(0, self.poll_time - overhead_time))), self._continuous_read_callback)
+    def _roll_buffers(self, n_read):
+        self._data_buffer = np.roll(self._data_buffer, -n_read)
+        self._timestamp_buffer = np.roll(self._timestamp_buffer, -n_read)
+        self._current_buffer_position -= n_read
+
+    # DataInStreamInterface
+    constraints = OverloadedAttribute()
+    @constraints.overload("DataInStreamInterface")
+    @property
+    def constraints(self) -> DataInStreamConstraints:
+        if self._constraints is None:
+            raise ValueError("Constraints have not yet been initialized.")
+        return self._constraints
+
+    def start_stream(self) -> None:
+        """ Start the data acquisition/streaming """
+        if self.module_state() == 'idle':
+            self.module_state.lock()
+            with self._lock:
+                self._prepare_buffers()
+            self._start_continuous_read()
+        else:
+            self.log.warning('Unable to start input stream. It is already running.')
+
+    def stop_stream(self) -> None:
+        """ Stop the data acquisition/streaming """
+        self.log.debug("Requested stop.")
+        if self.module_state() == 'locked':
+            self._stop_continuous_read()
+            self.log.debug("unlocking")
+            self.module_state.unlock()
+        else:
+            self.log.warning('Unable to stop wavemeter input stream as nothing is running.')
+
+    def read_data_into_buffer(self,
+                              data_buffer: np.ndarray,
+                              samples_per_channel: int,
+                              timestamp_buffer: Optional[np.ndarray] = None) -> None:
+        """ Read data from the stream buffer into a 1D numpy array given as parameter.
+        Samples of all channels are stored interleaved in contiguous memory.
+        In case of a multidimensional buffer array, this buffer will be flattened before written
+        into.
+        The 1D data_buffer can be unraveled into channel and sample indexing with:
+
+            data_buffer.reshape([<samples_per_channel>, <channel_count>])
+
+        The data_buffer array must have the same data type as self.constraints.data_type.
+
+        In case of SampleTiming.TIMESTAMP a 1D numpy.float64 timestamp_buffer array has to be
+        provided to be filled with timestamps corresponding to the data_buffer array. It must be
+        able to hold at least <samples_per_channel> items:
+
+        This function is blocking until the required number of samples has been acquired.
+        """
+        if self.module_state() != 'locked':
+            raise RuntimeError('Unable to read data. Stream is not running.')
+        while self.available_samples < samples_per_channel:
+            time.sleep(self.poll_time)
+        with self._lock:
+            n_read = min(self._current_buffer_position, samples_per_channel)
+            data_buffer[:n_read] = self._data_buffer[:n_read]
+            if timestamp_buffer is not None:
+                timestamp_buffer[:n_read] = self._timestamp_buffer[:n_read]
+            self._roll_buffers(n_read)
+
+    def read_available_data_into_buffer(self,
+                                        data_buffer: np.ndarray,
+                                        timestamp_buffer: Optional[np.ndarray] = None) -> int:
+        """ Read data from the stream buffer into a 1D numpy array given as parameter.
+        All samples for each channel are stored in consecutive blocks one after the other.
+        The number of samples read per channel is returned and can be used to slice out valid data
+        from the buffer arrays like:
+
+            valid_data = data_buffer[:<channel_count> * <return_value>]
+            valid_timestamps = timestamp_buffer[:<return_value>]
+
+        See "read_data_into_buffer" documentation for more details.
+
+        This method will read all currently available samples into buffer. If number of available
+        samples exceeds buffer size, read only as many samples as fit into the buffer.
+        """
+        if self.module_state() != 'locked':
+            raise RuntimeError('Unable to read data. Stream is not running.')
+        with self._lock:
+            if timestamp_buffer is None:
+                raise RuntimeError(
+                    'SampleTiming.TIMESTAMP mode requires a timestamp buffer array'
+                )
+            n_read = min(self._current_buffer_position, len(data_buffer))
+            data_buffer[:n_read] = self._data_buffer[:n_read]
+            timestamp_buffer[:n_read] = self._timestamp_buffer[:n_read]
+            self._roll_buffers(n_read)
+            return n_read
+
+    def read_data(self,
+                  samples_per_channel: Optional[int] = None
+                  ) -> Tuple[np.ndarray, Union[np.ndarray, None]]:
+        """ Read data from the stream buffer into a 1D numpy array and return it.
+        All samples for each channel are stored in consecutive blocks one after the other.
+        The returned data_buffer can be unraveled into channel samples with:
+
+            data_buffer.reshape([<samples_per_channel>, <channel_count>])
+
+        The numpy array data type is the one defined in self.constraints.data_type.
+
+        In case of SampleTiming.TIMESTAMP a 1D numpy.float64 timestamp_buffer array will be
+        returned as well with timestamps corresponding to the data_buffer array.
+np.concatenate(np.array([self._time_offset]), wrapped)
+TypeError: only integer scalar arrays can be converted to a scalar index
+        If samples_per_channel is omitted all currently available samples are read from buffer.
+        This method will not return until all requested samples have been read or a timeout occurs.
+        """
+        if self.module_state() != 'locked':
+            raise RuntimeError('Unable to read data. Stream is not running.')
+        if samples_per_channel is None:
+            samples_per_channel = self.available_samples
+        while self.available_samples < samples_per_channel:
+            self.log.debug("Zzzzz")
+            time.sleep(self.poll_time)
+        self.log.debug("Awaiting lock")
+        with self._lock:
+            n_read = min(self._current_buffer_position, samples_per_channel)
+            data_buffer = np.empty(n_read,dtype=np.float64)
+            timestamp_buffer = np.empty(n_read,dtype=np.float64)
+            data_buffer[:n_read] = self._data_buffer[:n_read]
+            timestamp_buffer[:n_read] = self._timestamp_buffer[:n_read]
+            self.log.debug("rolling")
+            self._roll_buffers(n_read)
+            return (data_buffer, timestamp_buffer)
+
+    def read_single_point(self) -> Tuple[np.ndarray, Union[None, np.float64]]:
+        """ This method will initiate a single sample read on each configured data channel.
+        In general this sample may not be acquired simultaneous for all channels and timing in
+        general can not be assured. Us this method if you want to have a non-timing-critical
+        snapshot of your current data channel input.
+        May not be available for all devices.
+        The returned 1D numpy array will contain one sample for each channel.
+
+        In case of SampleTiming.TIMESTAMP a single numpy.float64 timestamp value will be returned
+        as well.
+        """
+        if self.module_state() != 'locked':
+            raise RuntimeError('Unable to read data. Stream is not running.')
+        with self._lock:
+            f = self._get_current()
+            if f is None:
+                return np.empty(0),None
+            try:
+                return np.array(float(f)),None 
+            except ValueError:
+                return np.empty(0), None
+
+    @property
+    def available_samples(self) -> int:
+        with self._lock:
+            return self._current_buffer_position
+
+    @property
+    def sample_rate(self) -> float:
+        return 1/self.poll_time
+
+    @property
+    def channel_buffer_size(self) -> int:
+        return self.buffer_size
+
+    @property 
+    def streaming_mode(self) -> StreamingMode:
+        return StreamingMode.CONTINUOUS
+
+    @property
+    def active_channels(self):
+        return ['current']
+
+    def configure(self,
+                  active_channels: Sequence[str],
+                  streaming_mode: Union[StreamingMode, int],
+                  channel_buffer_size: int,
+                  sample_rate: float) -> None:
+        """ Configure a data stream. See read-only properties for information on each parameter. """
+        with self._lock:
+            self.buffer_size = channel_buffer_size
+            self._prepare_buffers()
+            self.poll_time = 1/sample_rate
 
     # SwitchInterface
     @property
@@ -165,6 +424,7 @@ class MOGLABSMotorizedLaserDriver(SwitchInterface, ProcessControlInterface):
         """
         return True
 
+    @constraints.overload("ProcessControlInterface")
     @property
     def constraints(self):
         """ Read-Only property holding the constraints for this hardware module.
